3349581adb85b0b3bb9971c7144d162965b2852f
--global <dylanrhinehart@gmail.com>
2024-03-03 19:59:16 -0800

    fixed socials

--- app/_components/socials.tsx
+++ app/_components/socials.tsx
@@ -8,7 +8,7 @@ function Socials() {
   return (
     <div className="flex flex-col socials">
         <a href="https://discord.gg/2Vq2uz7HEg"><RxDiscordLogo color={"white"} size={40} className="icon"/></a>
-        <a><LiaTelegramPlane color={"white"} size={40} className="icon"/></a>
+        {/* <a><LiaTelegramPlane color={"white"} size={40} className="icon"/></a> */}
         <a href="https://twitter.com/GalactixSol"><RiTwitterXLine color={"white"} size={40} className="icon"/></a>
     </div>
   )
